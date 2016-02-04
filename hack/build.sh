ce0c9ea84e6bc85a76f7c9342505e0d391a281ed
Yaroslav Molocko <onorua@gmail.com>
2016-02-04 14:55:36 +0800

    fix typo

--- hack/build.sh
+++ hack/build.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f "${BASH_SOURCE[0]}")
 cd $(dirname $SCRIPT)
 mkdir -p ../_out
 rm -f ../_out/*
-docker build -t 'seewas-dev:master'  ../
+docker build -t 'seesaw-dev:master'  ../
 echo $HACK_DIR
-docker run -i -t --rm=true -v $(readlink -f ../_out):/go/bin/out seewas-dev:master
+docker run -i -t --rm=true -v $(readlink -f ../_out):/go/bin/out seesaw-dev:master
 cd $OLD_PWD
